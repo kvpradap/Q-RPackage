6986c5bc45802633108d328ace35ae7f70aae1d5
pradap <pradap@cs.wisc.edu>
2015-04-12 14:05:25 -0500

    Updated block_attr_equivalence routine

--- QPackage/R/attr-equivalence-blocking.R
+++ QPackage/R/attr-equivalence-blocking.R
@@ -22,14 +22,47 @@ create_inv_index <- function(table_obj, col_name) {
   
 }
 
+# helper function to get_pairs 
+# given a vector, computes n choose 2 pairs and return it as data frame
+fn <- function(d) {
+  df <- data.frame()
+  r <- combn(d, 2, FUN=function(x) { df <<- rbind(x, df) })
+  colnames(df) <- c("id1", "id2")
+  df
+}
+
 # function to get candidate pairs
 
 get_pairs <- function(inv_index) {
   
   # consider inv index with # ids > 1
-  inv_index <- inv_index[lapply(tt, length) > 1]
+  inv_index <- inv_index[lapply(inv_index, length) > 1]
   
-  # 
+  # store current setting of stringsAsFactors 
+  str_opt <- getOption("stringsAsFactors")
+  options(stringsAsFactors = F)
   
-   
+  df_idx <- lapply(inv_index, FUN=fn)
+  kk <- unname(df_idx)
+  
+  df <- data.frame()
+  
+  # merge all the dataframes
+  for(i in 1:length(kk)) {
+    df <- rbind(df, kk[[i]])
+  }
+  
+  # reset back stringsAsFactors option
+  options(stringsAsFactors=str_opt)
+  
+  df
+  
+}
+
+block_attr_equivalence <- function(table_obj, col_name) {
+  if(nchar(table_obj@idCol) == 0) {
+    stop("id col is empty")
+  }
+  inv_index <- create_inv_index(table_obj, col_name)
+  cand_set <- get_pairs(inv_index)
 }
--- QPackage/R/test.R
+++ QPackage/R/test.R
@@ -1,13 +1,16 @@
 # -- import data
 d <- ReadCsv('../QPackage/data/DBLP_cleaned.csv', idCol="id")
 
-d_sample = QTable(d[1:10, ])
+d_sample <- QTable(d[1:10, ])
 
-d_sample = SetId(d_sample, d@idCol)
+d_sample <- SetId(d_sample, d@idCol)
 
-inv_index <- create_inv_index(d_sample, "year")
+#inv_index <- create_inv_index(d_sample, "year")
 
-inv_index <- inv_index[lapply(inv_index, length) > 1]
+#inv_index <- inv_index[lapply(inv_index, length) > 1]
+
+#cand_set <- get_pairs(inv_index)
+df <- block_attr_equivalence(d_sample, "year")
 
 
 
