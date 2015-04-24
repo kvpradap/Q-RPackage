4771d08ea8024558dffa0a4809096a6761d184df
kvpradap <pradap@cs.wisc.edu>
2015-04-23 22:30:18 -0500

    Added code
    added test code

---
+++ QPackage/R/test.R
@@ -0,0 +1,95 @@
+# -- import data
+#<<<<<<< HEAD
+# d <- ReadCsv('../QPackage/data/DBLP_cleaned.csv', idCol="id")
+# 
+# d_sample = QTable(d[1:10, ])
+# 
+# d_sample = SetId(d_sample, d@idCol)
+# 
+# # inv_index <- create_inv_index(d_sample, "year")
+# # 
+# # inv_index <- inv_index[lapply(inv_index, length) > 1]
+# # 
+# # qtable <- function(df) {
+# #   structure(list(df, key=c()), class=c("foo", "data.frame"))
+# # }
+# temp <- 5
+# ref <- function (temporari) 
+# {
+#   subx <- substitute(temporari)
+#   if (is.name(subx)) 
+#     subx <- deparse(subx)
+#   if (!is.character(subx)) 
+#     stop("'function' requires a name")
+#   parent <- parent.frame()
+#   if (exists(subx, envir = parent, inherits = TRUE)) 
+#     x <- 10
+#   else {
+#     #stop('variable not defined')
+#     x <- 35
+#     environment(x) <- parent
+#   
+#   
+#   }
+#   assign(subx, x, envir = parent)
+# }
+# =======
+# d <- ReadCsv('../QPackage/data/DBLP_cleaned.csv', idCol="id")
+# 
+# d_sample <- QTable(d[1:10, ])
+# 
+# d_sample <- SetId(d_sample, d@idCol)
+
+#inv_index <- create_inv_index(d_sample, "year")
+
+#inv_index <- inv_index[lapply(inv_index, length) > 1]
+
+#cand_set <- get_pairs(inv_index)
+#df <- block_attr_equivalence(d_sample, "year")
+
+
+
+# fn <- function(n, p,  block_attr1, block_attr2) {
+#   if(n %in% block_attr2 == TRUE) {
+#     i <- grep(n, block_attr2)
+#     n <- block_attr1[i]
+#     return(n)
+#   }
+#   
+#   if(n %in% block_attr1) {
+#     return(n)
+#   }
+#   
+#   return(paste(n,p, sep=""))
+#   
+# }
+
+
+
+
+
+if(0) {
+  fn <- function(a, b) { return(2*a + b)}
+  assign("jac_1", fn)
+  
+  
+  
+  get_fn_list <- function() {
+    list_fns <- list()
+    
+    
+    fn <- function(a, b) { return(a +b)}
+    assign(deparse(substitute(s)), fn)
+    #list_fns[[1]] <- as.symbol(deparse(substitute(s)))
+    list_fns[["jac_1"]] <- fn
+    
+    s <- "jac_2"
+    fn <- function(a, b) { return(a - b)}
+    assign(deparse(substitute(s)), fn)
+    #list_fns[[2]] <- as.symbol(deparse(substitute(s)))
+    list_fns[["jac_2"]] <- fn
+    return(list_fns)
+  }
+  eval(parse(text = "fn <- function(a, b) { return (a * b) } "))
+  
+}
